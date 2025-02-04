c263a32cf8b680fe61c11096a90987706e75076b
shorifuzzaman <156690506+Shorifuzzaman-Git@users.noreply.github.com>
2025-02-04 22:36:15 +0600

    Add files via upload

---
+++ job_scheduling.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct job
+{
+    int id;
+    int deadline;
+    int profite;
+};
+
+int comparator(job a,job b){
+    return a.profite>b.profite;
+}
+
+void func(job arr[],int n,int max_deadline){
+    vector<int>solution(max_deadline+1,-1);
+    int count=0,profitSum=0;
+    for(int i=0;i<n;i++){
+        if(solution[arr[i].deadline]==-1){
+            solution[arr[i].deadline]=arr[i].profite;
+            profitSum+=arr[i].profite;
+            count++;
+        }
+        else{
+            for(int j=arr[i].deadline;j>0;j--){
+                if(solution[j]==-1){
+                    solution[j]=arr[i].profite;
+                    profitSum+=arr[i].profite;
+                    count++;
+                    break;
+                }
+            }
+        }
+    }
+    cout<<count<<endl;
+    cout<<profitSum<<endl;
+}
+
+
+int main() {
+    int n;
+    cin>>n;
+    vector<int>id(n),deadline(n),profite(n);
+
+    for(int i=0;i<n;i++)cin>>id[i];
+    for(int i=0;i<n;i++)cin>>deadline[i];
+    for(int i=0;i<n;i++)cin>>profite[i];
+
+    job arr[n];
+    for(int i=0;i<n;i++){
+        arr[i]={id[i],deadline[i],profite[i]};
+    }
+
+    int max_deadline=*max_element(deadline.begin(),deadline.end());
+
+    sort(arr,arr+n,comparator);
+    func(arr,n,max_deadline);
+}
+
+
+// 4
+// 1 2 3 4
+// 4 1 1 1
+// 20 1 40 30
+
+// 5
+// 1 2 3 4 5
+// 2 1 2 1 1
+// 100 19 27 25 15
